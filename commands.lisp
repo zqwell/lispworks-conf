806179cd772e9ff264dfac2a5a6592d3d755ed39
zqwell <zqwell.ss@gmail.com>
2014-09-24 01:25:20 +0900

    Added `Move Over ()' command.

--- commands.lisp
+++ commands.lisp
@@ -1,6 +1,6 @@
 ;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: CL-USER; Base: 10 -*-
 
-(in-package :editor)
+(in-package :cl-user)
 
 ;; http://permalink.gmane.org/gmane.lisp.lispworks.general/671
 ;;(editor:defcommand "Delete Comment Prefix in Region" (p)
@@ -23,8 +23,26 @@
                 ;; move a point to end of command prefix
                 (loop while (eq (editor:character-at end1 0) #\;)
                       do (editor:character-offset end1 1))
-                (unless (point= end1 start)
+                (unless (editor:point= end1 start)
                   (when (eq (editor:character-at end1 0) #\Space) ; remove separating space
                     (editor:character-offset end1 1)))          ; if any
                 (editor:delete-between-points start end1))      ; delete the comment prefix
               (editor:line-offset start 1))))))                 ; move to next line
+
+(editor:defcommand "Move Over ()" (p)
+  "Move past the next close parenthesis.
+Any indentation preceeding the parenthesis is deleted."
+  "Move past the next close parenthesis."
+  (declare (ignore p))
+  (let ((point (editor:current-point)))
+    (editor:with-point ((m point))
+      (cond ((editor::forward-up-list m)
+	     (editor:move-point point m)
+             (editor::point-before point)
+             (loop (editor:with-point ((back point))
+                     (editor::back-to-indentation back)
+                     (unless (editor:point= back point)
+                       (return)))
+                   (editor::delete-indentation point))
+	     (editor::point-after point))
+	    (t (editor:editor-error))))))
